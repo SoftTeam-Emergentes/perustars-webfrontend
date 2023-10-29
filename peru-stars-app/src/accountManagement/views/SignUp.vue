77ed231dab58c22c6a1825006abfd93b57b0ea09
Rojoxt <delamarzp@gmail.com>
2023-10-29 10:10:14 -0500

    feat: add sign-up component

---
+++ peru-stars-app/src/accountManagement/views/SignUp.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import ToolBar from "@/shared/components/tool-bar.vue";
+</script>
+
+<template>
+  <header>
+    <tool-bar></tool-bar>
+  </header>
+<h1>SignUp component</h1>
+</template>
+
+<style scoped>
+
+</style>
