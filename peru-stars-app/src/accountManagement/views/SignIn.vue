aac95a894f48ee60a78ba2664560e44f6d50ea87
Rojoxt <delamarzp@gmail.com>
2023-10-29 10:10:08 -0500

    feat: add sign-in component

---
+++ peru-stars-app/src/accountManagement/views/SignIn.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import ToolBar from "@/shared/components/tool-bar.vue";
+</script>
+
+<template>
+  <header >
+    <tool-bar></tool-bar>
+  </header >
+  <body >
+  <h1>sign in component</h1>
+  </body>
+
+</template>
+
+<style scoped>
+
+</style>
