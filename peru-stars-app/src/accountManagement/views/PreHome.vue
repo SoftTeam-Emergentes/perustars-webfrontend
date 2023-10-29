3f539224bfac52dfb80afad1e8bdf2456ac4481c
Rojoxt <delamarzp@gmail.com>
2023-10-29 10:10:41 -0500

    feat: add home welcome component

---
+++ peru-stars-app/src/accountManagement/views/PreHome.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import ToolBar from "@/shared/components/tool-bar.vue";
+</script>
+
+<template>
+  <tool-bar></tool-bar>
+<h1>hello word</h1>
+</template>
+
+<style scoped>
+
+</style>
