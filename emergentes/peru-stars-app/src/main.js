9224ee72604da51952188a1eaffd0ef37afe2d78
Rojoxt <delamarzp@gmail.com>
2023-10-30 00:23:58 -0500

    fear: add primevue theme

--- emergentes/peru-stars-app/src/main.js
+++ emergentes/peru-stars-app/src/main.js
@@ -2,13 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+//Add PrimeFlex
+
+//Add Theme
+import 'primevue/resources/themes/lara-light-teal/theme.css'
+//Component
+import Button from "primevue/button";
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.component('Button', Button);
 app.use(createPinia())
 app.use(router)
-
+app.use(PrimeVue);
 app.mount('#app')
