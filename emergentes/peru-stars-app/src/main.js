b0efcf5feee114121408c71c9572a6e812de9669
Rojoxt <delamarzp@gmail.com>
2023-10-30 00:29:59 -0500

    fear: add primeIcon

--- emergentes/peru-stars-app/src/main.js
+++ emergentes/peru-stars-app/src/main.js
@@ -2,19 +2,23 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+//Add PrimeVue
 import PrimeVue from 'primevue/config';
+
 //Add PrimeFlex
-import "primevue/resources/themes/lara-light-blue/theme.css";
+import 'primeflex/primeflex.css';
 
 //Add Theme
 import 'primevue/resources/themes/lara-light-teal/theme.css'
 import "primevue/resources/primevue.min.css";
+import 'primeicons/primeicons.css'
 //Component
 
 
 
-import App from './App.vue'
-import router from './router'
+
 
 const app = createApp(App)
 
