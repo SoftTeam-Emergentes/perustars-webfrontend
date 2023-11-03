01c08195af3f2cd6c322f67e1214217679e8053b
Rojoxt <delamarzp@gmail.com>
2023-11-02 20:25:08 -0500

    fix: fixed top artist component
     page

--- emergentes/peru-stars-app/src/profileManagement/components/top-artist.vue
+++ emergentes/peru-stars-app/src/profileManagement/components/top-artist.vue
@@ -1,18 +1,18 @@
 
 
 <template>
-  <div class="w-full">
-    <pv-carousel :value="products" :numVisible="3" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="3000">
+  <div >
+    <div class="mx-6 my-4 text-2xl font-semibold mb-3">Artists</div>
+    <pv-carousel :value="artists" :numVisible="3" :numScroll="1" :responsiveOptions="responsiveOptions" circular :autoplayInterval="3000"  >
       <template #item="slotProps">
-        <div class="border-1 surface-border border-round m-2 text-center py-5 px-3">
+        <div class="border-1 surface-border border-round m-2 text-center py-5 px-3 shadow-5">
           <div class="mb-3">
-            <img :src="'https://primefaces.org/cdn/primevue/images/product/' + slotProps.data.image" :alt="slotProps.data.name" class="w-6 shadow-2" />
+            <img src="https://cdn-icons-png.flaticon.com/512/2815/2815428.png" :alt="slotProps.data.brandname" class="w-6 shadow-2" />
           </div>
           <div>
-            <h4 class="mb-1">{{ slotProps.data.name }}</h4>
-            <h6 class="mt-0 mb-3">${{ slotProps.data.price }}</h6>
-            <Tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data.inventoryStatus)" />
-            <div class="mt-5 flex align-items-center justify-content-center gap-2">
+            <h5 class="mb-1">{{ slotProps.data.firstname }}</h5>
+            <h6 class="mt-0 mb-3">{{ slotProps.data.description }}</h6>
+             <div class="mt-5 flex align-items-center justify-content-center gap-2">
               <pv-button icon="pi pi-search" rounded />
               <pv-button  icon="pi pi-star-fill" rounded severity="secondary" />
             </div>
@@ -21,21 +21,58 @@
       </template>
     </pv-carousel>
 
-
-    <h1>top artist</h1>
-
   </div>
 
 
 </template>
 
 <script>
+import {ArtistsApiService} from "@/profileManagement/services/artist-api.service";
+
 export default {
   name: "top-artist",
+  artistService:null,
   data(){
     return{
-
+      artists:{},
+      responsiveOptions: [
+        {
+          breakpoint: '1400px',
+          numVisible: 2,
+          numScroll: 1
+        },
+        {
+          breakpoint: '1199px',
+          numVisible: 3,
+          numScroll: 1
+        },
+        {
+          breakpoint: '767px',
+          numVisible: 2,
+          numScroll: 1
+        },
+        {
+          breakpoint: '575px',
+          numVisible: 1,
+          numScroll: 1
+        }
+      ]
     }
+  },
+
+  created() {
+    this.getTopArtist()
+  },
+
+  methods:{
+    getTopArtist() {
+      this.artistService=  new ArtistsApiService();
+      this.artistService.getAll().then((response)=>{
+        this.artists=response.data;
+        console.log()
+      })
+    },
+
   }
 
 }
--- emergentes/peru-stars-app/src/profileManagement/views/HomeHobbyist.vue
+++ emergentes/peru-stars-app/src/profileManagement/views/HomeHobbyist.vue
@@ -1,28 +1,59 @@
-<script>
-import SideBarHobbyist from "@/profileManagement/components/side-bar-hobbyist.vue";
-import TopArtist from "@/profileManagement/components/top-artist.vue";
 
-export default {
-  name: "HomeHobbyist",
-  components: {TopArtist, SideBarHobbyist}
-}
-</script>
 
 <template>
 
   <body>
-  <div class="flex flex-row">
-    <side-bar-hobbyist></side-bar-hobbyist>
-    <top-artist></top-artist>
-    <h1>home hobbyist</h1>
+  <div class="flex flex-row ">
+    <div v-if="screenIsWide" >
+      <side-bar-hobbyist ></side-bar-hobbyist>
+    </div>
+    <div class=" overflow-auto">
+      <top-artist></top-artist>
+      <top-art></top-art>
+    </div>
+
   </div>
-
-
 
   </body>
 
 
 </template>
+
+<script>
+import SideBarHobbyist from "@/profileManagement/components/side-bar-hobbyist.vue";
+import TopArtist from "@/profileManagement/components/top-artist.vue";
+import TopArt from "@/profileManagement/components/top-artwork.vue";
+
+export default {
+  name: "HomeHobbyist",
+  components: {TopArt, TopArtist, SideBarHobbyist},
+
+  data(){
+    return{
+      screenIsWide: true,
+    }
+
+  },
+  mounted() {
+    // Escuchar el evento de cambio de tamaño de la pantalla
+    window.addEventListener("resize", this.checkScreenWidth);
+
+    // Llamar a la función para verificar el ancho de pantalla al cargar la página
+    this.checkScreenWidth();
+  },
+  methods: {
+    checkScreenWidth() {
+      // Verificar el ancho de la pantalla y actualizar la variable
+      this.screenIsWide = window.innerWidth >= 700;
+    },
+  },
+  beforeUnmount() {
+    // Asegúrate de eliminar el oyente del evento antes de destruir el componente
+    window.removeEventListener("resize", this.checkScreenWidth);
+  },
+
+}
+</script>
 
 <style scoped>
 
