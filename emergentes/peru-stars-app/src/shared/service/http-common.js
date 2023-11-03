c62401e55b69121f1f80a84bb4f907d2dd1d5b16
Rojoxt <delamarzp@gmail.com>
2023-11-02 20:24:26 -0500

    feat: add common service
     page

--- emergentes/peru-stars-app/src/shared/service/http-common.js
+++ emergentes/peru-stars-app/src/shared/service/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //API runs in port 3000, consider this for all services.js
 export default axios.create({
-    baseURL: 'https://localhost:3000/api/v1',
+    baseURL: 'http://localhost:3000/api/v1',
     headers: { 'Content-type': 'application/json' }
 })
